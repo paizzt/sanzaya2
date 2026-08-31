a9acdc33f5c28e5d0fafe7bbc39d949d9febf641
paizzt <martevenfaisal@gmail.com>
2026-08-31 10:24:51 +0800

    UI: reduce top spacing in PaymentRequests Index

--- resources/js/Pages/PaymentRequests/Index.jsx
+++ resources/js/Pages/PaymentRequests/Index.jsx
@@ -66,7 +66,7 @@ export default function Index({ auth, paymentRequests, summary, filters, isAppro
         >
             <Head title={pageTitle} />
 
-            <div className="py-6">
+            <div className="py-2">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     {/* Dashboard Summary Cards - Only show on Request View */}
                     {!isApprovalView ? (
