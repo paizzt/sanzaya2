6c595a1156dbeb61c594b220ddb966a7f088a4fe
paizzt <martevenfaisal@gmail.com>
2026-09-08 10:27:03 +0800

    fix: Import ApplicationLogo via Vite for correct subfolder asset resolution

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,8 +1,10 @@
+import logo from '../assets/logo.png';
+
 export default function ApplicationLogo(props) {
     return (
         <img
             {...props}
-            src="/img/logo.png"
+            src={logo}
             alt="Application Logo"
         />
     );
