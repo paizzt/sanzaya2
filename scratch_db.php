4cbe1608f26066c63ec4bd4e44a0b2eb280ab3b4
paizzt <martevenfaisal@gmail.com>
2026-09-18 17:51:56 +0800

    chore: clean up scratch files

--- scratch_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$r = Illuminate\Support\Facades\DB::table('sync_logistik_data')->select('tanggal', 'sheet_name')->take(10)->get();
-echo json_encode($r, JSON_PRETTY_PRINT);
