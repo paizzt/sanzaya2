8293212585973fc8fb89c126ab1e6029d711f6f2
paizzt <martevenfaisal@gmail.com>
2026-09-18 18:06:34 +0800

    fix: bug parameter saat klik Tambah Target Baru di modal

--- resources/js/Pages/Company/Index.jsx
+++ resources/js/Pages/Company/Index.jsx
@@ -469,7 +469,7 @@ export default function Index({ auth, companies, companyTargets = [], is_super_a
                         <>
                             <div className="flex justify-between items-center mb-4">
                                 <h4 className="font-semibold text-gray-700">Daftar Grup Target</h4>
-                                <PrimaryButton onClick={() => openTargetModal({})} className="bg-blue-600 hover:bg-blue-700">
+                                <PrimaryButton onClick={() => openTargetModal()} className="bg-blue-600 hover:bg-blue-700">
                                     <Plus className="w-4 h-4 mr-2" />
                                     Tambah
                                 </PrimaryButton>
