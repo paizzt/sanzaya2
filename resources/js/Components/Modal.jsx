f9ffde0f4c4a3a5aad77969953bdc9dd8c46574e
paizzt <martevenfaisal@gmail.com>
2026-09-25 15:17:49 +0800

    Make all modals globally responsive and internally scrollable

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -56,7 +56,7 @@ export default function Modal({
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                     <DialogPanel
-                        className={`mb-6 transform overflow-visible rounded-lg bg-white shadow-xl transition-all w-full sm:mx-auto ${maxWidthClass}`}
+                        className={`mb-6 transform overflow-y-auto max-h-[90vh] rounded-lg bg-white shadow-xl transition-all w-full sm:mx-auto ${maxWidthClass}`}
                     >
                         {children}
                     </DialogPanel>
